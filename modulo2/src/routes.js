f85fc2fbee679255e1691f6cc3e13f0f47eada69
maccali <guilherme.maccali@universo.univates.br>
2019-10-29 17:03:52 -0300

    Validação de agendamentos

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -19,6 +20,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
