1b85aba875ac72eb3ece679fbdba59476f509d37
maccali <guilherme.maccali@universo.univates.br>
2019-10-30 15:09:08 -0300

    Listando agenda do prestador

--- modulo2/src/app/controllers/ScheduleController.js
+++ modulo2/src/app/controllers/ScheduleController.js
@@ -1,3 +1,6 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -12,8 +15,20 @@ class ScheduleController {
     }
 
     const { date } = req.query;
+    const parseDate = parseISO(date);
 
-    return res.json({ date });
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
   }
 }
 
