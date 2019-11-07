594e10350a2ac25a023aadf2cb3c3759bfe4e04a
maccali <guilherme.maccali@universo.univates.br>
2019-11-07 12:02:01 -0300

    Até o router

--- modulo5/src/App.js
+++ modulo5/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
-  return (
-    <div className="App">
- kk
-    </div>
-  );
+    return <Routes />;
 }
 
 export default App;
