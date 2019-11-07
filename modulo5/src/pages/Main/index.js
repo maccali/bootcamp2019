d10ebb0aac81a324fe6c2188b2e530ab67df2e1f
maccali <guilherme.maccali@universo.univates.br>
2019-11-07 16:07:25 -0300

    Estilizando página Main

--- modulo5/src/pages/Main/index.js
+++ modulo5/src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-    return <Title>Hello world</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adiconar Repositório" />
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
