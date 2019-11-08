75425095470b30d5a0061d1cc9eeae53b67f0abf
maccali <guilherme.maccali@universo.univates.br>
2019-11-08 14:49:00 -0300

    Carrigando da api com Promise

--- modulo5/src/pages/Repository/index.js
+++ modulo5/src/pages/Repository/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../servises/api';
 
-// import { Container } from './styles';
+class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
-export default function Repository({ match }) {
-  return (
-    <>
-      <div>Repository {decodeURIComponent(match.params.repository)}</div>
-    </>
-  );
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+    console.log(repository);
+    console.log(issues);
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>FDS</h1>;
+  }
 }
+
+export default Repository;
