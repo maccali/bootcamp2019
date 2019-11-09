6078a112c03aa99d8e0711200129f750505d8e12
maccali <guilherme.maccali@universo.univates.br>
2019-11-08 22:52:38 -0300

    Começo do modulo de react native

---
+++ modulo6/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Oi?</Text>
+      <Text>Oi?</Text>
+      <Text>Oi?</Text>
+    </View>
+  );
+}
